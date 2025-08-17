2fed730f5cf095596be1e0e49e7a2342d4b3b771
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-08-17 16:35:59 +0200

    Clean code

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -1,14 +1,3 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
-
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
-
-// Add your code right below, good luck!
-
 const firstName = "Carlos";
 const lastName = "Stevenson";
 const thisYear = 1965;
