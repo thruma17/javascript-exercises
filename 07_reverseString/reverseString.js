8aec287ca7655983f81bf80498bddaf990d30240
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-09-23 19:46:14 +0200

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  array = Array.from(string);
+  reversedArray = array.reverse();
+  reversedString = reversedArray.join(``);
+  return reversedString;
 };
+
+reverseString(`hello there`);
 
 // Do not edit below this line
 module.exports = reverseString;
