ff43d5c0407e0c8f0aece64931e10a2271c8630c
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-09-23 18:39:19 +0200

    Complete 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return `ERROR`;
+  }
 
+  let word = ``;
+
+  for (let index = 0; index < num; index++) {
+    word += string;
+  }
+  return word;
 };
 
 // Do not edit below this line
