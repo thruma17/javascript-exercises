78c317c131bf604691483b5424875d94aa87ad15
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-09-23 20:22:34 +0200

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  return array.filter((val) => !args.includes(val));
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
