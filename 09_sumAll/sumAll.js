66b632a41a6dc5a96745d9325d43187b0c0246fa
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-09-24 11:51:24 +0200

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b) && a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return `ERROR`;
+  }
 };
 
 // Do not edit below this line
