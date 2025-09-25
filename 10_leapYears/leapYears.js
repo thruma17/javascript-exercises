2f28a21a2832a66d8d7f03f3f989c43a3ff0bb31
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-09-25 10:30:16 +0200

    Complete 10_leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (isItLeap) {
+  if (isItLeap % 4 === 0 && (isItLeap % 100 !== 0 || isItLeap % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+leapYears(2000); // is a leap year: returns true
+leapYears(1985); // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
