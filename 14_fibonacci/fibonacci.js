2078b4a0bbd34b42f782f1095dea61d299484b00
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-30 12:15:47 +0100

    Complete "fibonacci" first 6 tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num === 0) {
+    return 0;
+  }
+  let prev = 1;
+  let prevPrev = 0;
+  for (let i = 2; i <= num; i++) {
+    let curr = prev + prevPrev;
+    prevPrev = prev;
+    prev = curr;
+  }
+  return prev;
 };
 
 // Do not edit below this line
