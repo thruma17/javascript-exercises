8105f59e30ddc8b65673575e11823e0b44e044e8
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-30 19:41:16 +0100

    Complete "findTheOldest" tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    if (oldestAge < currentAge) {
+      return current;
+    } else {
+      return oldest;
+    }
+  });
 };
 
 // Do not edit below this line
